2984a19131bfe1f9705a4f3d15e3c9d824d37848
mfrakhman <rrrfajar1@gmail.com>
2024-03-13 21:53:03 +0700

    first deployment on vercel2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require("./models");
 const userRoute = require("./routes/user.router");
 const productRoute = require("./routes/product.router");
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
@@ -17,12 +17,7 @@ db.sequelize.sync({ force: true }).then(() => {
   console.log("db has been re sync");
 });
 
-app.use("/", (req, res) => {
-  res.send("hello");
-});
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server is connected on ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server is connected on ${PORT}`));
